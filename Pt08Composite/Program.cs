5a20daaf605e84f782149babe48e3f2b5157ed67
soljen <soljen@gmail.com>
2015-10-19 23:32:33 +0300

    Composite.

---
+++ Pt08Composite/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt08Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Суть Composite - работа с деревьями
+
+            Console.ReadLine();
+        }
+    }
+}
