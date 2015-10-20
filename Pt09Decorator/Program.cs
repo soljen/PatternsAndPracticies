5ed6ea00d6e73512d365f44c02cdd02289270654
soljen <soljen@gmail.com>
2015-10-21 00:15:03 +0300

    Decorator.

---
+++ Pt09Decorator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt09Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Суть Decorator - декорирование объектов свойствами/объектами/компанентами, как новыми возможностями/функциями
+
+            Console.ReadLine();
+        }
+    }
+}
