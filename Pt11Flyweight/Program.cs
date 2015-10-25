d2eea9d351e504410a31b3bf8d9bb1a47ead96d4
soljen <soljen@gmail.com>
2015-10-25 19:05:06 +0200

    Flyweight.

---
+++ Pt11Flyweight/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt11Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Суть паттерна - резделение всех объектов на разделяемые и неразделяемые.
+
+            Console.ReadLine();
+        }
+    }
+}
