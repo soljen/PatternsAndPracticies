124d08d987d4f7d5b858964146ef4bbb5b14a698
soljen <soljen@gmail.com>
2015-10-26 23:24:45 +0200

    Proxy.

---
+++ Pt12Proxy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt12Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Некая прослойка, между клиентом и сервисом, в каком-то смысле скрывающая от клиента реализацию самого сервиса.
+
+            Console.ReadLine();
+        }
+    }
+}
