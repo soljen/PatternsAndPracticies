3998d0b3361ee9f94e3ca177eb61d886c6e75e19
soljen <soljen@gmail.com>
2015-10-28 20:33:54 +0200

    Command.

---
+++ Pt14Command/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pt14Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Объект как очередь некоторых действий
+
+            Console.ReadLine();
+        }
+    }
+}
