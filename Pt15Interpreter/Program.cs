522b45197a399a44a5e38f001044cc232e18c200
soljen <soljen@gmail.com>
2015-10-29 07:50:48 +0200

    Interpreter.

---
+++ Pt15Interpreter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt15Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // для описания языков и языковых конструкций
+
+            Console.ReadLine();
+        }
+    }
+}
