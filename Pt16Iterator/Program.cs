7f93a5f260ee48a7a46add115a75ffab71d98627
soljen <soljen@gmail.com>
2015-11-01 19:50:01 +0200

    Iterator.

---
+++ Pt16Iterator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt16Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Итератор, курсор, реализация коллекций
+
+            Console.ReadLine();
+        }
+    }
+}
