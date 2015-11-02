ea4f2560c1e8b42d7634068536811a17e3db9c92
soljen <soljen@gmail.com>
2015-11-02 23:08:17 +0200

    Mediator.

---
+++ Pt17Mediator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt17Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //
+
+            Console.ReadLine();
+        }
+    }
+}
