58ccbafda8348dda3ff1313725ced51be5db427f
soljen <soljen@gmail.com>
2015-11-04 23:24:21 +0200

    Memento.

---
+++ Pt18Memento/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt18Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Для мгновенного сохранения состояния объекта, и мгновенного восстановления состояния
+
+            Console.ReadLine();
+        }
+    }
+}
