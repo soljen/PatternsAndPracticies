b984508ce588c2246145e4a05fa5abe1c1cca330
soljen <soljen@gmail.com>
2015-11-08 21:25:10 +0200

    Observer.

---
+++ Pt19Observer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt19Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // События и делегаты
+
+            Console.ReadLine();
+        }
+    }
+}
