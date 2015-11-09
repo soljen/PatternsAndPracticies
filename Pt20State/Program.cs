92d7a63e86d60f00e189f75c98a99e2486a490fa
soljen <soljen@gmail.com>
2015-11-09 23:43:56 +0200

    State.

---
+++ Pt20State/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt20State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // машина сстояний
+
+            Console.ReadLine();
+        }
+    }
+}
