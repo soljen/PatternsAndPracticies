907f4f471637c95abf47faec0798500711bb97cb
soljen <soljen@gmail.com>
2015-11-11 20:27:01 +0200

    Template method.

---
+++ Pt22TemplateMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt22TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // задаёт некоторый алгоритм построения объекта, скрывая внутреннюю реализацию - абтрагирование.
+
+            Console.ReadLine();
+        }
+    }
+}
