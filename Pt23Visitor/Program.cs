31d248a82d6a1c31f7a2fa1af84b2f7398327664
soljen <soljen@gmail.com>
2015-11-12 20:39:37 +0200

    Visitor.

---
+++ Pt23Visitor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pt23Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //
+
+            Console.ReadLine();
+        }
+    }
+}
